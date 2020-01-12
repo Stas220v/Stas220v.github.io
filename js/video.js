a817979c823a02c2b2341df52493f26581e2b796
Stas220v <47213538+Stas220v@users.noreply.github.com>
2020-01-13 01:54:08 +0200

    Update video.js

--- js/video.js
+++ js/video.js
@@ -4,13 +4,9 @@ function pl(el){
 	bc.style.visibility = "visible";
 	bc.style.opacity = "1";
 	player.src=el.id;
-	player.play();
 }
 function st(){
 	bc.style.opacity = "0";
 	bc.style.visibility = "hidden";
-	if(player==document.pictureInPictureElement){
-		document.exitPictureInPicture();
-	}	
-	player.pause();
+	player.src="";
 }
